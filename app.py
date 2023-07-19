345f0409de7fea59f057a91657704f6d37a045fc
Bernardo Andrade <berdfandrade@gmail.com>
2023-07-19 15:09:17 -0300

    --resolvendo dploy

---
+++ app.py
@@ -0,0 +1,34 @@
+
+import json
+from flask import Flask, jsonify
+from criar_monstro import gerar_monstro
+from criar_heroi import gerar_heroi
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/', methods=['GET'])
+def get_hello():
+    mensagem = { "messagem" : "Use a rota GET /monstro, para gerar um monstro, e a rota GET /heroi, para gerar um heroi!"}
+    return jsonify(mensagem), 200
+
+
+@app.route('/monstro', methods=['GET'])
+def get_monstro():
+    monstro = gerar_monstro()
+    monstro_JSON = json.loads(monstro)
+    return jsonify(monstro_JSON), 200
+
+
+@app.route('/heroi', methods=['GET'])
+def get_heroi():
+    heroi = gerar_heroi()
+    heroi_JSON = json.loads(heroi)
+    return jsonify(heroi_JSON), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+# IMG HANDLE; 
+
---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Ativação de ambiente virtual (se aplicável)
+# source venv/bin/activate
+
+# Instalação das dependências
+pip install -r requirements.txt
+
+# Configuração de variáveis de ambiente (se necessário)
+# export VARIAVEL=valor
+
+# Execução da API
+python api.py
+
